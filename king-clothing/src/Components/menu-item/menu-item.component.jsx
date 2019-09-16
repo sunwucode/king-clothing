0f5c702386f5bde8c2c492f5b529757aece7e5f3
sunwucode <wukongkhan8@gmail.com>
2019-09-16 21:11:53 +0200

    refactor

--- king-clothing/src/Components/menu-item/menu-item.component.jsx
+++ king-clothing/src/Components/menu-item/menu-item.component.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const MenuItem = ({title}) => (
-  <div className="menu-item">
+const MenuItem = ({title, imageUrl, size}) => (
+  <div style={{
+    backgroundImage: `url(${imageUrl})`
+  }} className={`${"size"} menu-item`}>
         <div className="content">
           <h1 className="title">{title}</h1>
           <span className="subtitle">Shop now !</span>
