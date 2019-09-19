564100dc1b6cfcf89fd62a643107087dfce9b632
sunwucode <wukongkhan8@gmail.com>
2019-09-19 12:38:41 +0200

    debugging and styling

--- king-clothing/src/Components/directory/directory.component.jsx
+++ king-clothing/src/Components/directory/directory.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
 
+import "./directory.styles.scss";
+
 class Directory extends React.Component {
   constructor() {
     super();
@@ -45,8 +47,8 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
           {
-            this.state.sections.map(({title, imageUrl, id}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl}/>
+            this.state.sections.map(({title, imageUrl, id, size}) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
             ))
           }
       </div>
