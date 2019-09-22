a657eabe92a7c6a3196191b3cdf991d6c98aa5f6
sunwucode <wukongkhan8@gmail.com>
2019-09-22 15:31:53 +0200

    updadting collection item and previex components with style

---
+++ king-clothing/src/Components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
+import "./preview-collection.component.scss";
+
+const CollectionPreview = ({items, title}) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview"> 
+        {
+          items
+          .filter((item, idx) => idx <4)
+          .map(({id, ...otheritemprops}) => (
+            <CollectionItem key={id} {...otheritemprops} />
+          ))
+        }    
+    </div>
+  </div>
+)
+
+export default CollectionPreview;
