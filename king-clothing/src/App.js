b20fa82bce38f79db8df757e257ab825a04f530c
sunwucode <wukongkhan8@gmail.com>
2019-09-28 23:37:50 +0200

    lot of auth with firebase

--- king-clothing/src/App.js
+++ king-clothing/src/App.js
@@ -25,8 +25,18 @@ class App extends React.Component{
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {})
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {console.log(this.state)})
+        });
+                
       }
+
+      else {this.setState({currentUser: userAuth})};
   });
     
   }
--- king-clothing/src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ king-clothing/src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-import SignIn from "../../Components/sign-in/sign-in.component"
+import SignIn from "../../Components/sign-in/sign-in.component";
+import SignUp from "../../Components/sign-up/sign-up.component";
+
 import "./sign-in-and-sign-up.styles.scss";
 
 
 const SignInAndSignUp = () => (
   <div className="sign-in-and-sign-up">
     <SignIn/>
+    <SignUp/>
   </div>
   
 );
