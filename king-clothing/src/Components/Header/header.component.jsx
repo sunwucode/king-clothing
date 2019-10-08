452177bf371078aa2fe9fb1e67898c93a51a351e
sunwucode <wukongkhan8@gmail.com>
2019-10-08 15:52:06 +0200

    selectors done but still buggy

--- king-clothing/src/Components/Header/header.component.jsx
+++ king-clothing/src/Components/Header/header.component.jsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 
 import { connect } from "react-redux";
 
+import { createStructuredSelector } from "reselect";
+
 import { auth } from "../../firebase/firebase.utils";
 
 import CartIcon from "../cart-icon/cart-icon.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import Cart from "../cart-dropdown/cart-dropdown.component"
+import Cart from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import "./header.styles.scss";
 
@@ -34,9 +38,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
--- king-clothing/src/redux/cart/cart.selectors.js
+++ king-clothing/src/redux/cart/cart.selectors.js
@@ -7,6 +7,11 @@ export const selectCartItems = createSelector(
   cart => cart.Items
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
 export const selectCartItemsCount = ()=> createSelector(
   [selectCartItems],
   cartItems =>
